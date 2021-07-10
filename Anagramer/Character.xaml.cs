b3992fe99c5658fffeffdc7bc829b42d4769ede6
burneech <81528115+burneech@users.noreply.github.com>
2021-07-10 17:37:39 +0200

    Added drag and drop functionality

--- Anagramer/Character.xaml.cs
+++ Anagramer/Character.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
 
 namespace Anagramer
 {
@@ -7,14 +9,57 @@ namespace Anagramer
     /// </summary>
     public partial class Character : UserControl
     {
-        private string _displayedCharacter;
-
         /// <inheritdoc cref="Character"/>
         /// <param name="displayCharacter">Character/letter/number to be displayed</param>
         public Character(string displayedCharacter)
         {
             InitializeComponent();
-            _displayedCharacter = TextBox_DisplayedCharacter.Text = displayedCharacter;
+            TextBox_DisplayedCharacter.Text = displayedCharacter;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DataObject data = new DataObject();
+                data.SetData("Object", this);
+
+                DragDrop.DoDragDrop(this, data, DragDropEffects.Move);
+            }
+        }
+
+        protected override void OnDragOver(DragEventArgs e)
+        {
+            base.OnDragOver(e);
+            e.Effects = DragDropEffects.None;
+
+            // Source/sender character object
+            Character _sourceCharacter = (Character)e.Data.GetData("Object");
+            if (_sourceCharacter != null)
+            {
+                e.Effects = DragDropEffects.Move;
+            }
+
+            e.Handled = true;
+        }
+
+        private void Character_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Handled == false)
+            {
+                // Source/sender character object
+                Character _sourceCharacter = (Character)e.Data.GetData("Object");
+
+                if (_sourceCharacter != null)
+                {
+                    // Switch source and target character
+                    string originalSourceCharacter = _sourceCharacter.TextBox_DisplayedCharacter.Text;          // Keep original temporarily
+                    _sourceCharacter.TextBox_DisplayedCharacter.Text = this.TextBox_DisplayedCharacter.Text;    // Source <= Target
+                    this.TextBox_DisplayedCharacter.Text = originalSourceCharacter;                             // Target => Source (original)
+                }
+            }
         }
     }
 }
